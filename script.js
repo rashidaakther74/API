15199910afff2b75298e4ba18b7e426adf8478b1
rashidarashida <rashidaakther774@gmail.com>
2025-07-26 23:11:27 -0700

    API-Cards

---
+++ script.js
@@ -0,0 +1,21 @@
+async function loadData() {
+  try {
+    let store = await fetch("https://fakestoreapi.com/products")
+      .then(res => res.json());
+
+    console.log(store);
+
+    document.getElementById("textAndHtml").innerHTML = store
+      .map((data) => `
+        <div class="card">
+          <p class="title">Title: ${data.title}</p>
+          <p class="price">Price: $${data.price}</p>
+        </div>
+      `)
+      .join("");
+  } catch (error) {
+    console.log(error, "Failed");
+  }
+}
+
+loadData();
